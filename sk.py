deb6ac66c86b79b5d11e680c5ab0991bb43e811c
Viola011 <egor.lomakin2008@mail.ru>
2025-01-20 01:10:55 +0300

    first

--- sk.py
+++ sk.py
@@ -2,3 +2,4 @@ s = input()if len(s) == 9 or len(s) ==10:
     if s[0] in "АВЕКМНОРСТУХ" and s[1:4].isdigit() and s[4] in 'АВЕКМНОРСТУХ' and s[5] in "АВЕКМНОРСТУХ" and s[6] == "_" and s[7:].isdigit():        print("YES")
     else:        print("NO")
 else:    print("NO")
+print('hszxfcgvhjk')
